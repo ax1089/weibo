195f19e1cf816ac548380ad6b32f4075a8e41190
wencai159@126.com <wencai159@126.com>
2020-11-07 22:42:24 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
     //
     public function create(){
         return view('sessions.create');
@@ -20,7 +29,8 @@ class SessionsController extends Controller
         if (Auth::attempt($credentials,$request->has('remember'))) {
             //登录成功后相关操作
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            $fallback = route('users.show',Auth::user());
+            return redirect()->intended($fallback);
 
         }else{
 
@@ -29,8 +39,6 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
 
         }
-
-        return;
     }
 
     public function destory(){
