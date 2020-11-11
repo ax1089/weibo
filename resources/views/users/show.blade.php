cded7436084c420bbe238186323941278f7c70f7
wencai159@126.com <wencai159@126.com>
2020-11-11 23:54:05 +0800

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,9 +4,11 @@
 @section('content')
     <div class="row">
         <div class="offset-md-2 col-md-8">
-                <div class="offset-md-2 col-md-8">
                     <section class="user_info">
                         @include('shared._user_info', ['user' => $user])
+                    </section>
+                    <section class="stats mt-2">
+                        @include('shared._stats',['user'=>$user])
                     </section>
                     <section class="status">
                         @if($statuses->count() > 0)
@@ -23,7 +25,6 @@
                         @endif
 
                     </section>
-                </div>
         </div>
     </div>
 @stop
