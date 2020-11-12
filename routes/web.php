16e7e82ac8cd245da7649d74776b27ccac97b0e8
wencai159@126.com <wencai159@126.com>
2020-11-12 10:47:26 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,11 @@ Route::post('password/reset','Auth\ResetPasswordController@reset')->name('passwo
 //微博 CRUD
 Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
 
+//关注人列表
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
+
+//粉丝列表
+Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');
 
 
 
